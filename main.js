9e0d02934a3268b2f459f55666b544880f448441
Kai Hendry <hendry@webconverger.com>
2013-02-25 12:01:26 +0800

    move reporting features to another branch

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-var videos = ["a.webm", "b.webm", "e.webm", "d.webm"];
+var videos = ["a.webm", "b.webm", "c.webm", "d.webm"];
 
-function downloadUrl(url, callback) {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
-			callback(request.responseText, request.status);
-		}
-	};
-	request.open('GET', url, true);
-	request.send(null);
-}
-
+// Play videos one after another in a loop
 var n = 0;
 var video = document.createElement('video');
 document.body.appendChild(video);
@@ -24,21 +14,11 @@ video.addEventListener('ended', function() {
 });
 video.play();
 
-if (!window.applicationCache) {
-	setTimeout(function() {
-		_gaq.push(['_trackEvent', 'Appcache', 'Version', 'n/a']);
-	},
-	1000);
-	document.body.style.background = "red";
-}
-
-var appCache = window.applicationCache;
-
 // "Naturally" reload when an update is available
 window.applicationCache.addEventListener('updateready', function() {
 	//if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
 		console.log("Reloading...");
-		try { window.applicationCache.swapCache(); } catch(e) {}
+		try { window.applicationCache.swapCache(); } catch(e) {} // This might fail on FF
 		location.reload();
 	//}
 },
@@ -52,41 +32,23 @@ setInterval(function() {
 	}
 },
 1000 * 60 * 1); // Every minute check for an update
-// Use GA to track the update rate of this manifest appcache thing
-// and see how fast users are updated to the latest cache/version
-if (typeof _gaq != 'undefined') window.addEventListener('load', function() {
-	setTimeout(function() {
-		var r = new XMLHttpRequest();
-		r.open('GET', 'manifest.appcache', true);
-		r.onload = function() {
-			var text = this.responseText;
-			if (!text) return;
-			var version = (text.match(/#\s\d[^\n\r]+/) || [])[0];
-			if (version) _gaq.push(['_trackEvent', 'Appcache', 'Version', version.replace(/^#\s/, '')]);
-		};
-		r.send();
-	},
-	1000);
-},
-false);
 
-//if (appCache.status === 0) document.body.style.background = "red"; // Firefox doesn't do this correctly
+//if (applicationCache.status === 0) document.body.style.background = "red"; // Firefox doesn't do this correctly
 
-// Cached event is fired when initially caching
-appCache.addEventListener('cached', function() {
+// Cached event is fired when initially caching or after failed previous caching
+applicationCache.addEventListener('cached', function() {
 	// Firefox has incorrect values of applicationCache.status, so we are are not checking
 	document.body.style.background = "green";
-	location.reload();
 },
 false);
 
-// No update is fired when an existing Appcache is found to have no update
-appCache.addEventListener('noupdate', function(e) {
+// No update is fired when an existing applicationCache is found to have no update
+applicationCache.addEventListener('noupdate', function(e) {
 	console.log("noupdate");
 	console.log(e);
-	console.log(appCache.status);
+	console.log(applicationCache.status);
 	document.body.style.background = "green";
-	//if (appCache.status == 1) {
+	//if (applicationCache.status == 1) {
 	//}
 	// Firefox has incorrect values of applicationCache.status
 	//else { // On first load applicationCache.status===applicationCache.UNCACHED, so cached event should take precendence
@@ -95,30 +57,32 @@ appCache.addEventListener('noupdate', function(e) {
 },
 false);
 
+// DEBUG
+
 function handleCacheEvent(e) {
 	console.log(e);
-	console.log(appCache.status);
+	console.log(applicationCache.status);
 }
 function handleCacheError(e) {
 	console.log('Error: Cache failed to update!');
 }
 
 // Fired after the first cache of the manifest.
-appCache.addEventListener('cached', handleCacheEvent, false);
+applicationCache.addEventListener('cached', handleCacheEvent, false);
 
 // Checking for an update. Always the first event fired in the sequence.
-appCache.addEventListener('checking', handleCacheEvent, false);
+applicationCache.addEventListener('checking', handleCacheEvent, false);
 
 // An update was found. The browser is fetching resources.
-appCache.addEventListener('downloading', handleCacheEvent, false);
+applicationCache.addEventListener('downloading', handleCacheEvent, false);
 
 // The manifest returns 404 or 410, the download failed,
 // or the manifest changed while the download was in progress.
-appCache.addEventListener('error', handleCacheError, false);
+applicationCache.addEventListener('error', handleCacheError, false);
 
 // Fired after the first download of the manifest.
-// appCache.addEventListener('noupdate', handleCacheEvent, false);
+// applicationCache.addEventListener('noupdate', handleCacheEvent, false);
 // Fired if the manifest file returns a 404 or 410.
 // This results in the application cache being deleted.
-appCache.addEventListener('obsolete', handleCacheEvent, false);
+applicationCache.addEventListener('obsolete', handleCacheEvent, false);
 
